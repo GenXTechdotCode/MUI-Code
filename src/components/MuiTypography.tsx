a5192e2cafbf05e3e487ea234bd0516b3fea8dce
GenXTechdotCode <142334403+GenXTechdotCode@users.noreply.github.com>
2023-09-30 22:27:32 +0800

    Add button

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -9,6 +9,16 @@ export const MuiTypography = () => {
         <Typography variant="h4">H1 heading</Typography>
         <Typography variant="h5">H1 heading</Typography>
         <Typography variant="h6">H1 heading</Typography>
+
+        <Typography variant="subtitle1">Sub Title 1</Typography>
+        <Typography variant="subtitle2">Sub Title 1</Typography>
+
+        <Typography variant="body1">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus nulla pariatur dicta sapiente. Maiores suscipit eos pariatur ut itaque et architecto ea molestiae commodi aliquam ratione, ducimus consequatur fugiat voluptate.
+        </Typography>
+        <Typography variant="body2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil atque distinctio magni, sequi fugiat possimus enim officiis perspiciatis, vero beatae, dolorum ratione fugit esse ipsum debitis saepe quaerat sed eos?
+            </Typography>
     </div>
   )
 }
