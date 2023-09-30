b9d738b831abd360e68d656426fce6457cf0c0ed
GenXTechdotCode <142334403+GenXTechdotCode@users.noreply.github.com>
2023-10-01 01:23:33 +0800

    Add alert click

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,4 +1,5 @@
-import { Stack, Button } from "@mui/material"
+import { Stack, Button, IconButton } from "@mui/material"
+import SendIcon from '@mui/icons-material/Send';
 
 export const MuiButton = () => {
   return (
@@ -16,7 +17,20 @@ export const MuiButton = () => {
             <Button variant='contained' color="info">Info</Button>
             <Button variant='contained' color="success">Success</Button>
         </Stack>
+
+        <Stack display={'block'} spacing={2} direction='row'>
+            <Button variant='contained' color="primary" size="small" disableElevation>Primary</Button>
+            <Button variant='contained' color="secondary" size="medium">Secondary</Button>
+            <Button variant='contained' color="error" size="large">Error</Button>
+            
+        </Stack>
         
+        <Stack spacing={2} direction='row'>
+            <Button variant='contained'  startIcon={<SendIcon />} disableElevation onClick={() => alert('napindot ko siya yehey')}>Send</Button>
+            <Button variant='contained'  endIcon={<SendIcon />} disableRipple>Send</Button>
+            <IconButton aria-label='send' color="success" size="small"><SendIcon /></IconButton>
+            
+        </Stack>
      </Stack>
   )
 }
