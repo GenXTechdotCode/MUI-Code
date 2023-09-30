56ae85f62caca151ebfa403ab579e04f0134813f
GenXTechdotCode <142334403+GenXTechdotCode@users.noreply.github.com>
2023-09-30 19:58:37 +0800

    my first MUI code

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>`
+        <Typography variant="h1">H1 heading</Typography>
+        <Typography variant="h2">H1 heading</Typography>
+        <Typography variant="h3">H1 heading</Typography>
+        <Typography variant="h4">H1 heading</Typography>
+        <Typography variant="h5">H1 heading</Typography>
+        <Typography variant="h6">H1 heading</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
